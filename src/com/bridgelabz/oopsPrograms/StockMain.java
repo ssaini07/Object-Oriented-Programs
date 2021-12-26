6b3a22ac5e0af7bfcda8941937b123fb3018e638
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-26 16:25:02 +0530

    shwoing welcome message

---
+++ src/com/bridgelabz/oopsPrograms/StockMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.oopsPrograms;
+
+public class StockMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to object oriented programs");
+
+	}
+
+}
